711e0ec4dcfaed502450b468ac3f76a5de16e847
lusl <luisa.zintgraf@hotmail.de>
2013-09-03 10:21:59 +0200

    Luisa tests Git! :)

--- Autonomous Agents/src/Initialiser.java
+++ Autonomous Agents/src/Initialiser.java
@@ -1,10 +1,11 @@
 
-public class Initialiser {
-
+public class Initialiser 
+{
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		System.out.println("Hello World!");
 	}
 
