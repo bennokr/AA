1a8428a50266bfa93c3e4b34bcd965d7c9077343
tvkanters <tvkanters@gmail.com>
2013-09-03 10:27:14 +0200

    Conflict test for Luisa

--- Autonomous Agents/src/Initialiser.java
+++ Autonomous Agents/src/Initialiser.java
@@ -1,12 +1,10 @@
-
-public class Initialiser 
-{
+public class Initialiser {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) 
-	{
+	public static void main(String[] args) {
 		System.out.println("Hello World!");
+		int i = 6; // i should never be 5!
 	}
 
 }
