ca73ee1760e0aa8fe3cc7ac92cb129a7bac67a31
bennokr <bennokr@gmail.com>
2013-10-17 17:54:34 +0200

    Speeding up LPsolve

--- Autonomous Agents/src/com/uva/aa/agents/MinimaxQ.java
+++ Autonomous Agents/src/com/uva/aa/agents/MinimaxQ.java
@@ -3,6 +3,9 @@ package com.uva.aa.agents;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import lpsolve.LpSolve;
 import lpsolve.LpSolveException;
@@ -13,41 +16,52 @@ import com.uva.aa.enums.Action;
 import com.uva.aa.policies.Policy;
 
 public class MinimaxQ {
-	
+
 	/* The zero-sum game payoff matrix per state
 	 * or Double Q[s][o][a]
 	 * or Q :: s -> o -> a -> Double
 	 */
 	private final HashMap<State, HashMap<Action, HashMap<Action, Double>>> stateGameValues;
-	
+
 	/*
 	 * The zero-sum game theoretical opponent 
 	 */
 	private Agent opponent;
-	
+
 	double alpha;
 	double decay;
-	
+	LpSolve solver;
+
 	protected MinimaxQ(Agent opponent, double alpha, double decay) {
-        // initialize Q(s,a,o)
-        stateGameValues = new HashMap<State, HashMap<Action, HashMap<Action, Double>>>();
-        this.alpha = alpha;
-        this.decay = decay;
-        this.opponent = opponent;
+		// initialize Q(s,a,o)
+		stateGameValues = new HashMap<State, HashMap<Action, HashMap<Action, Double>>>();
+		this.alpha = alpha;
+		this.decay = decay;
+		this.opponent = opponent;
+		try {
+			// Initialize immense overcapabilities
+			solver = LpSolve.makeLp(0, Action.values().length);
+			solver.setVerbose(0);
+			solver.setMaxim();
+			// Sum of probability distribution pi should be 1
+			solver.addConstraint(new double[]{1, 1, 1, 1, 1}, LpSolve.EQ, 1);
+		} catch (LpSolveException e) {
+			e.printStackTrace();
+		}
 	}
-	
+
 	/*
 	 * Update policy with Minimax-Q
 	 * 
 	 */
 	protected void learn(State initialState, State resultingState, Action previousAction, double reward, Policy policy) {	
-//		for(Action a : Action.values()) {
-//			System.out.print(policy.getActionProbability(initialState, a) + " ");
-//		}
-//		System.out.println();
-		
-		
-		
+		//		for(Action a : Action.values()) {
+		//			System.out.print(policy.getActionProbability(initialState, a) + " ");
+		//		}
+		//		System.out.println();
+
+
+
 		// Init Q
 		if (!stateGameValues.containsKey(initialState)) {
 			stateGameValues.put(initialState, new HashMap<Action, HashMap<Action, Double>>());
@@ -55,7 +69,7 @@ public class MinimaxQ {
 		if (!stateGameValues.get(initialState).containsKey(opponent.getLastAction())) {
 			stateGameValues.get(initialState).put(opponent.getLastAction(), new HashMap<Action, Double>());
 		}
-		
+
 		// Q(s,a,o) = (1-alpha) * Q(s,a,o) + alpha * (r + gamma * V(s'))
 		double Q = 0;
 		if (stateGameValues.get(initialState).get(opponent.getLastAction()).containsKey(previousAction)) {
@@ -64,20 +78,15 @@ public class MinimaxQ {
 		stateGameValues.get(initialState).get(opponent.getLastAction()).put(previousAction, 
 				(1-alpha) * Q 
 				+ alpha * (reward + Config.DISCOUNT_FACTOR_GAMMA * policy.getStateValue(resultingState)));
-		
+
 		// Minimize, starting with infinity
-		double R = Double.POSITIVE_INFINITY;
-		double[] pi = new double[Action.values().length];
-		for(Action o : Action.values()) {
-			if (!stateGameValues.get(initialState).containsKey(o)) {
-				stateGameValues.get(initialState).put(o, new HashMap<Action, Double>());
-			}
-			try {
-				// Initialize immense overcapabilities
-				LpSolve solver = LpSolve.makeLp(0, Action.values().length);
-				solver.setVerbose(0);
-				// Sum of probability distribution pi should be 1
-				solver.addConstraint(new double[]{1, 1, 1, 1, 1}, LpSolve.EQ, 1);
+		double minEndReward = Double.POSITIVE_INFINITY;
+		Set<double[]> bestPolicyArrays = new HashSet<double[]>();
+		try {
+			for(Action o : Action.values()) {
+				if (!stateGameValues.get(initialState).containsKey(o)) {
+					stateGameValues.get(initialState).put(o, new HashMap<Action, Double>());
+				}
 				// Java wants us to loop so we loop-de-loop
 				double[] Qs = new double[Action.values().length];
 				for (Action a : Action.values()) {
@@ -90,28 +99,29 @@ public class MinimaxQ {
 				}
 				// Maximize over innerproduct(Q(s,o), pi)
 				solver.setObjFn(Qs);
-				solver.setMaxim();
 				solver.solve();
 				
 				// Minimize outer loop
-				double newR = solver.getObjective();
-				double[] newPi = solver.getPtrVariables();
-				
-				if (newR < R) {
-					System.out.println(o.name());
-					R = newR;
-					pi = newPi.clone();
-				} else { System.err.println(o.name()); }
-			} catch (LpSolveException e) {
-				e.printStackTrace();
-			}
+				double endReward = solver.getObjective();
+				if (endReward <= minEndReward) {
+					minEndReward = endReward;
+					if (endReward < minEndReward) {
+						bestPolicyArrays.clear();
+					}
+					bestPolicyArrays.add(solver.getPtrVariables());
+				}
+			} 
+		} catch (LpSolveException e) {
+			e.printStackTrace();
 		}
 		for(Action a : Action.values()) {
-//			System.out.print(pi[a.ordinal()] + " ");
-			policy.setActionProbability(initialState, a, pi[a.ordinal()]);
+			double prob = 0;
+			for (double[] policyArray : bestPolicyArrays) {
+				prob += policyArray[a.ordinal()] ;
+			}
+			policy.setActionProbability(initialState, a, prob);
 		}
-//		System.out.println();
-		policy.setStateValue(resultingState, R);
+		policy.setStateValue(resultingState, minEndReward);
 		alpha = alpha * decay;
 	}
 }
