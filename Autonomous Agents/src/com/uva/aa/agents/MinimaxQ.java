e0f8a52e5d4d303d18a621374d33540f58e41826
bennokr <bennokr@gmail.com>
2013-10-18 11:14:07 +0200

    Implemented minimaxQ correctly, but it's still slow

--- Autonomous Agents/src/com/uva/aa/agents/MinimaxQ.java
+++ Autonomous Agents/src/com/uva/aa/agents/MinimaxQ.java
@@ -1,11 +1,6 @@
 package com.uva.aa.agents;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 import lpsolve.LpSolve;
 import lpsolve.LpSolveException;
@@ -38,11 +33,6 @@ public class MinimaxQ {
 		this.alpha = alpha;
 		this.decay = decay;
 		this.opponent = opponent;
-		try {
-
-		} catch (LpSolveException e) {
-			e.printStackTrace();
-		}
 	}
 
 	/*
@@ -50,14 +40,7 @@ public class MinimaxQ {
 	 * 
 	 */
 	protected void learn(State initialState, State resultingState, Action previousAction, double reward, Policy policy) {	
-		//		for(Action a : Action.values()) {
-		//			System.out.print(policy.getActionProbability(initialState, a) + " ");
-		//		}
-		//		System.out.println();
-
-
-
-		// Init Q
+		// Init action value map
 		if (!stateGameValues.containsKey(initialState)) {
 			stateGameValues.put(initialState, new HashMap<Action, HashMap<Action, Double>>());
 		}
@@ -66,7 +49,7 @@ public class MinimaxQ {
 		}
 
 		// Q(s,a,o) = (1-alpha) * Q(s,a,o) + alpha * (r + gamma * V(s'))
-		double Q = 0;
+		double Q = Config.DEFAULT_ACTION_VALUE;
 		if (stateGameValues.get(initialState).get(opponent.getLastAction()).containsKey(previousAction)) {
 			Q = stateGameValues.get(initialState).get(opponent.getLastAction()).get(previousAction);
 		}
@@ -74,16 +57,7 @@ public class MinimaxQ {
 				(1-alpha) * Q 
 				+ alpha * (reward + Config.DISCOUNT_FACTOR_GAMMA * policy.getStateValue(resultingState)));
 
-		// Minimize, starting with infinity
-		double minEndReward = Double.POSITIVE_INFINITY;
-		Set<double[]> bestPolicyArrays = new HashSet<double[]>();
-		for (Action o : Action.values()) {
-			if (!stateGameValues.get(initialState).containsKey(o)) {
-				stateGameValues.get(initialState).put(o, new HashMap<Action, Double>());
-			}
-		}
 		try {
-			// Initialize immense overcapabilities
 			// With 1 extra parameter for the value of the minimization
 			solver = LpSolve.makeLp(0, Action.values().length + 1);
 			solver.setVerbose(0);
@@ -91,26 +65,39 @@ public class MinimaxQ {
 			// Sum of probability distribution pi should be 1
 			solver.addConstraint(new double[]{1, 1, 1, 1, 1, 0}, LpSolve.EQ, 1);
 
-			// Java wants us to loop so we loop-de-loop
-			double[] Qs = new double[Action.values().length];
-			for (Action a : Action.values()) {
-				// Init action value if not present
-				if (!stateGameValues.get(initialState).get(o).containsKey(a)) {
-					stateGameValues.get(initialState).get(o).put(a, Config.DEFAULT_ACTION_VALUE);
+			for (Action o : Action.values()) {
+				// Init action value map if not present
+				if (!stateGameValues.get(initialState).containsKey(o)) {
+					stateGameValues.get(initialState).put(o, new HashMap<Action, Double>());
 				}
-				//solver.setColName(a.ordinal()+1, a.name());
-				Qs[a.ordinal()] = stateGameValues.get(initialState).get(o).get(a);
+
+				// Java wants us to loop so we loop-de-loop
+				double[] minimizationConstraint = new double[Action.values().length+1];
+				for (Action a : Action.values()) {
+					// Init action value if not present
+					if (!stateGameValues.get(initialState).get(o).containsKey(a)) {
+						stateGameValues.get(initialState).get(o).put(a, Config.DEFAULT_ACTION_VALUE);
+					}
+					minimizationConstraint[a.ordinal()] = - stateGameValues.get(initialState).get(o).get(a);
+				}
+				minimizationConstraint[Action.values().length] = 1;
+				solver.addConstraint(minimizationConstraint, LpSolve.EQ, 1);
 			}
+
+
 			// Maximize
-			solver.setObjFn(Qs);
+			solver.setObjFn(new double[]{0, 0, 0, 0, 0, 1});
 			solver.solve();
+			
+			// Update
+			for(Action a : Action.values()) {
+				policy.setActionProbability(initialState, a, solver.getPtrVariables()[a.ordinal()]);
+			}
+			policy.setStateValue(resultingState, solver.getObjective());
+			alpha = alpha * decay;
+			
 		} catch (LpSolveException e) {
 			e.printStackTrace();
 		}
-		for(Action a : Action.values()) {
-			policy.setActionProbability(initialState, a, solver.getPtrVariables()[a.ordinal()]);
-		}
-		policy.setStateValue(resultingState, solver.getObjective(););
-		alpha = alpha * decay;
 	}
 }
